70b25d8ad6e8147243a54910226c784ae01508a9
BronteBantam <jwilcock@gmail.com>
2017-05-10 13:41:26 -0400

    Initial Commit

---
+++ ProvincialParks.CampingApp/Infrastructure/DependencyInjectionBindings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject.Modules;
+using Ninject;
+using ProvincialParks.CampingApp.Infrastructure.Interfaces;
+
+namespace ProvincialParks.CampingApp.Infrastructure
+{
+    public class Bindings : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IFileReader>().To<TextFileReader>();
+            Bind<IFileWriter>().To<TextFileWriter>();
+            Bind<IExpenseCalculator>().To<CampingTripExpenseCalculator>();
+
+        }
+    }
+}
---
+++ ProvincialParks.CampingApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject;
+using ProvincialParks.CampingApp.Infrastructure.Interfaces;
+using System.Reflection;
+namespace ProvincialParks.CampingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kernel = new StandardKernel();
+            kernel.Load(Assembly.GetExecutingAssembly());
+            var expenseCalculator = kernel.Get<IExpenseCalculator>();
+
+            Console.WriteLine("Enter Filename");
+            var lineRead = Console.ReadLine();
+            if (!string.IsNullOrEmpty(lineRead))
+            {
+                expenseCalculator.CalculateExpenses(lineRead);
+            }
+            else
+            {
+                Console.WriteLine("Filename Entered Invalid");
+            }
+        }
+    }
+}
